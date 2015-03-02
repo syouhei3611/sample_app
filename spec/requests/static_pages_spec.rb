8375c4f4555a369375973ea125a568dc6307e5a6
syouhei3611 <syouhei3611@hotmail.com>
2015-03-02 02:29:36 +0000

    Improve static_pages_spec.rb

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,7 +2,9 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
-  describe "Home page" do
+	let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
+    describe "Home page" do
 
     it "should have the content 'Sample App'" do
     	visit '/static_pages/home'
@@ -11,7 +13,7 @@ describe "StaticPages" do
 
     it "should have the right title" do
     	visit'/static_pages/home'
-    	expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+    	expect(page).to have_title("#{base_title} | Home")
     end
   end
 
@@ -24,7 +26,7 @@ describe "StaticPages" do
 
     it "should have the right title" do
     	visit'/static_pages/help'
-    	expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+    	expect(page).to have_title("#{base_title} | Help")
     end
   end
 
@@ -37,7 +39,7 @@ describe "StaticPages" do
 
     it "should have the right title" do
     	visit'/static_pages/about'
-    	expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+    	expect(page).to have_title("#{base_title} | About Us")
     end
   end
 
@@ -50,7 +52,7 @@ describe "StaticPages" do
 
     it "should have the right title" do
     	visit'/static_pages/contact'
-    	expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
+    	expect(page).to have_title("#{base_title} | Contact")
     end
   end
 end
